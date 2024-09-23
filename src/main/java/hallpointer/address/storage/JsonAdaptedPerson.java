bf57311941a8d1456b181f7deede4df1b5c478ea
keithxun <keithonghx@gmail.com>
2024-09-23 20:38:24 +0800

    Complete updating of the tutorial-adding-command branch

--- src/main/java/hallpointer/address/storage/JsonAdaptedPerson.java
+++ src/main/java/hallpointer/address/storage/JsonAdaptedPerson.java
@@ -108,6 +108,10 @@ class JsonAdaptedPerson {
 
         final Set<Tag> modelTags = new HashSet<>(personTags);
 
+        if (remark == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Remark.class.getSimpleName()));
+        }
+
         final Remark modelRemark = new Remark("");
         return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags, modelRemark);
     }
